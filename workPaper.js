8aa5b4a5ae69d48520621cdbf54b8c7814c85de1
sauvepro <unsauvechriss@gmail.com>
2023-05-19 23:58:52 +0300

    work paper calculation

---
+++ workPaper.js
@@ -0,0 +1,16 @@
+function calculateBlankPagesNeeded(n, m) {
+    // Return 0 if n or m is less than 0
+    if (n < 0 || m < 0) {
+      return 0;
+    }
+    // Calculate the total number of pages needed
+    const totalPagesNeeded = n * m;
+  
+    // Calculate the number of blank pages needed
+    return totalPagesNeeded;
+  }
+  const numberOfClassmates = 5;
+const numberOfPages = 5;
+
+const blankPages = calculateBlankPagesNeeded(numberOfClassmates, numberOfPages);
+console.log("Number of blank pages needed:", blankPages);
